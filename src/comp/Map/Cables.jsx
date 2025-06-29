6f8bb7f594b6f5dfff95e14f2567035bec045be0
amranter200444@gmail.com <amranter200444@gmail.com>
2025-06-29 07:31:28 +0300

    .

--- src/comp/Map/Cables.jsx
+++ src/comp/Map/Cables.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import MapComp from '../Func/MapComp'
+import React from 'react';
+import MapComp from '../Func/MapComp';
 
 export default function Cables() {
   return (
-        <MapComp src={'../../../public/assets/map/cables.jpg'}   alt="#">
-  <ul className='list-disc '>
-<li>Wide-Grip Cable Attachment</li>
-<li>Close-Grip Cable Attachment</li>
-  </ul>
-</MapComp>
-  )
+    <MapComp src={'/assets/map/cables.jpg'} alt="#">
+      <ul className="list-disc">
+        <li>Wide-Grip Cable Attachment</li>
+        <li>Close-Grip Cable Attachment</li>
+      </ul>
+    </MapComp>
+  );
 }
+
