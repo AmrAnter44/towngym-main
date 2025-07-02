bb4487a46b0408b1442a658b263e61b668cf4d04
amranter200444@gmail.com <amranter200444@gmail.com>
2025-07-02 17:15:51 +0300

    .

--- src/comp/Map/Spa.jsx
+++ src/comp/Map/Spa.jsx
@@ -3,7 +3,7 @@ import MapComp from '../Func/MapComp'
 
 export default function Spa() {
   return (
-        <MapComp src="#" alt="oopps:(">
+       <MapComp src={'/assets/map/spa.jpg'} alt="oopps:(">
   <ul className='list-disc '>
         <p>"Sauna, steam, and jacuzzi — everything you need to relax and recharge after a powerful workout!"
 </p>
