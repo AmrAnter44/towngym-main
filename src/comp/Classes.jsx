9f4e5be02829fcb23a57ebf065b1ffaff27275eb
amranter200444@gmail.com <amranter200444@gmail.com>
2025-08-13 18:43:39 +0300

    ..

--- src/comp/Classes.jsx
+++ src/comp/Classes.jsx
@@ -5,15 +5,15 @@ export default function Home() {
     { id: 1, className: "Functional Training", day: "Saturday", time1: "8:00",coachName:"Zima",mix:"Mix" },
     { id: 2, className: "Aerobics", day: "Sunday", time1: "8:00",coachName:"Aimlia",mix:"Ladies" },
     { id: 3, className: "Flexibility", day: "Sunday", time1: "9:00",coachName:"Aimlia",mix:"Ladies" },
-    { id: 4, className: "Superman Kids", day: "Monday", time1: "6:00",coachName:"Zima",mix:"Mix",Mem:true  },
+    // { id: 4, className: "Superman Kids", day: "Monday", time1: "6:00",coachName:"Zima",mix:"Mix",Mem:true  },
     { id: 5, className: "Boxing", day: "Monday", time1: "7:00",coachName:"Saif",mix:"Mix" ,Mem:true },
     { id: 6, className: "Circuit", day: "Monday", time1: "8:00",coachName:"Menna",mix:"Ladies" },
     { id: 7, className: "Zumba", day: "Tuesday", time1: "8:00",coachName:"Menna",mix:"Ladies" },
     { id: 8, className: "Yoga", day: "Tuesday", time1: "9:00",coachName:"Aimlia",mix:"Mix" },
-    { id: 9, className: "Core", day: "Wednesday", time1: "8:00",coachName:"Zima",mix:"Mix" },
-    { id: 10, className: "Superman Kids", day: "Thursday", time1: "6:00",coachName:"Zima",mix:"Mix",Mem:true  },
-    { id: 11, className: "Boxing", day: "Thursday", time1: "7:00",coachName:"Saif",mix:"Mix",Mem:true  },
-    { id: 12, className: "Belly Dancing", day: "Sunday", time1: "8:00",coachName:"Samah",mix:"Ladies" },
+    { id: 9, className: "belly dance", day: "Wednesday", time1: "8:00",coachName:"menna",mix:"Mix" },
+    // { id: 10, className: "Superman Kids", day: "Thursday", time1: "6:00",coachName:"Zima",mix:"Mix",Mem:true  },
+    { id: 11, className: "core", day: "Thursday", time1: "8:00",coachName:"zima",mix:"Mix",Mem:true  },
+    // { id: 12, className: "Belly Dancing", day: "Sunday", time1: "8:00",coachName:"Samah",mix:"Ladies" },
   ]);
 
   return (
